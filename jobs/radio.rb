7f399bde8615516dd762756c46c34c346670a527
Andy Soell <andy@teamsoell.com>
2015-06-12 15:40:22 -0400

    Added 'previously played' information to radio widget

--- jobs/radio.rb
+++ jobs/radio.rb
@@ -15,5 +15,23 @@ SCHEDULER.every '1m', :first_in => 0 do |job|
     npartist = element.elements["artist"].text
   }
 
-  send_event('radio', { npcover: npcover, nptitle: nptitle, npartist: npartist})
+  response = http.request(Net::HTTP::Get.new("/xml/now_4.xml"))
+  i = 0
+  doc = REXML::Document.new(response.body)
+
+  upcoming = Hash.new({ value: 0 })
+  ppcover = ""
+  pptitle = ""
+  ppartist = ""
+  doc.elements.each('playlist/song') { |element|
+    i = i + 1
+    if i > 1
+      ppcover = element.elements["coverart"].text
+      pptitle = element.elements["title"].text
+      ppartist = element.elements["artist"].text
+      upcoming[i-1] = { cover: ppcover, title: pptitle, artist: ppartist }
+    end
+  }
+
+  send_event('radio', { npcover: npcover, nptitle: nptitle, npartist: npartist, upcoming: upcoming.values})
 end
