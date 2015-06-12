3dfa29361209066ef8c1a5a087930ccf2d2e4783
Andy Soell <andy@teamsoell.com>
2015-06-12 14:35:35 -0400

    Updated radio widget for Radio Paradise; Updated vertical layout

--- jobs/radio.rb
+++ jobs/radio.rb
@@ -1,20 +1,19 @@
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
 require 'net/http'
-require 'json'
+require 'rexml/document'
 SCHEDULER.every '1m', :first_in => 0 do |job|
-  http = Net::HTTP.new('radio.saltmines.us')
-  response = http.request(Net::HTTP::Get.new("/controller.php?r=getQueue"))
-  json = JSON.parse(response.body)
-  npcover = json['queue'][0]['bigIcon']
-  nptitle = json['queue'][0]['name']
-  npartist = json['queue'][0]['artist']  
-  
-  upcoming = Hash.new({ value: 0 }) 
-  json['queue'].length.times do |i|
-    if i>0 && i<6
-      upcoming[i-1] = { cover: json['queue'][i]['bigIcon'], title: json['queue'][i]['name'], artist: json['queue'][i]['artist'] }
-    end
-  end
+  http = Net::HTTP.new('radioparadise.com')
+  response = http.request(Net::HTTP::Get.new("/xml/now.xml"))
+  doc = REXML::Document.new(response.body)
 
-  send_event('radio', { npcover: npcover, nptitle: nptitle, npartist: npartist, upcoming: upcoming.values})
+  npcover = ""
+  nptitle = ""
+  npartist = ""
+  doc.elements.each('playlist/song') { |element|
+    npcover = element.elements["coverart"].text
+    nptitle = element.elements["title"].text
+    npartist = element.elements["artist"].text
+  }
+
+  send_event('radio', { npcover: npcover, nptitle: nptitle, npartist: npartist})
 end
