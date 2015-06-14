b90462c452fba357b38c00bb3a49e7fe557001a5
Will Koehler <wck555@gmail.com>
2015-06-13 23:22:02 -0400

    Update all the gems.
    Needed to fix this https://github.com/gitlabhq/gitlab-ci/issues/480. Latest Twitter gem had some breaking changes that required code updates.

--- jobs/twitter.rb
+++ jobs/twitter.rb
@@ -3,24 +3,22 @@ require 'twitter'
 
 #### Get your twitter keys & secrets:
 #### https://dev.twitter.com/docs/auth/tokens-devtwittercom
-Twitter.configure do |config|
+client = Twitter::REST::Client.new do |config|
   config.consumer_key = 'eWBpw4omptJd8ClCdKqQ'
   config.consumer_secret = 'lYXrxXEqylIAK1oihHNoPcDC7XfW1b2I0hVJ1PA'
-  config.oauth_token = '818615719-LbKZueyjpg4Sb40V8ds36x13HaWmRnaeqPMmyR06'
-  config.oauth_token_secret = 'JDX4PybWXfMfYNHZ30Q04b0xZRDa5pRGMZaM8KyXoybgT'
+  config.access_token = '818615719-LbKZueyjpg4Sb40V8ds36x13HaWmRnaeqPMmyR06'
+  config.access_token_secret = 'JDX4PybWXfMfYNHZ30Q04b0xZRDa5pRGMZaM8KyXoybgT'
 end
-
-search_term = URI::encode('@TheSaltMines')
 
 SCHEDULER.every '10m', :first_in => 0 do |job|
   begin
-    tweets = Twitter.search("#{search_term}").results
+    tweets = client.search(URI::encode('@TheSaltMines'))
 
-    if tweets
-      tweets.map! do |tweet|
-        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https.sub('normal','bigger') }
+    if tweets.count > 0
+      tweets_hash = tweets.map do |tweet|
+        { name: tweet.user.name, body: tweet.text, avatar: tweet.user.profile_image_url_https.to_s.sub('normal','bigger') }
       end
-      send_event('twitter_mentions', comments: tweets)
+      send_event('twitter_mentions', comments: tweets_hash)
     end
   rescue Twitter::Error
     puts "\e[33mFor the twitter widget to work, you need to put in your twitter API keys in the jobs/twitter.rb file.\e[0m"
