549b6c82b9975b2672ea2052679675f25948684c
Will Koehler <wck555@gmail.com>
2015-06-19 14:50:02 -0400

    Rewrite the bookings widget.
    - Use icalendar gem to parse ics file
    - Simplify display. Don't use different styles for today, this week, later
    - Clean up the way date and time is rendered.

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'dashing'
 gem 'xml-simple'
+gem 'icalendar'
+gem 'activesupport'   # needed so icalendar uses correct timezones
 
 ## Remove this if you don't need a twitter widget.
 gem 'twitter'
--- jobs/bookings.rb
+++ jobs/bookings.rb
@@ -1,79 +1,68 @@
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-require 'net/http'
-require 'date'
+require 'icalendar'
 
-SCHEDULER.every '1m', :first_in => 0 do |job|
-  cal_file=(Net::HTTP.get 'booking.saltmines.us', '/info/webcal/258B66.ics')
-  events = cal_file.split('BEGIN:')
-  
-  reDTSTART = /DTSTART;TZID=[^:]+:(\d\d\d\d\d\d\d\dT\d\d\d\d\d\d)/
-  reDTEND   = /DTEND;TZID=[^:]+:(\d\d\d\d\d\d\d\dT\d\d\d\d\d\d)/
-  reDTSTART_allday = /DTSTART;VALUE=DATE:(\d\d\d\d\d\d\d\d)/
-  reDTEND_allday   = /DTEND;VALUE=DATE:(\d\d\d\d\d\d\d\d)/    
-  
-  reSUMMARY = /SUMMARY:\s*(.*)\s*(DESCRIPTION|ORGANIZER)/
-  reDESCRIPTION = /DESCRIPTION:\s*(.*)\s*(CREATED|ORGANIZER)/  
-  
-  today = Hash.new({ value: 0 })   
-  thisweek = Hash.new({ value: 0 })     
-  later = Hash.new({ value: 0 })     
-  
-  i = 0
-  max_events = 4
-  events.each do |event|  
+class Bookings
 
-    dtstart     = reDTSTART.match event
-    dtend       = reDTEND.match event
-    summary     = reSUMMARY.match event
-    description = reDESCRIPTION.match event
-    
-    if dtstart 
+  def run
+    SCHEDULER.every '1m', :first_in => 0 do |job|
 
-    
-      dtstart = DateTime.parse(dtstart[1])
-      dtend   = DateTime.parse(dtend[1])
-
-      
-      if dtstart > DateTime.now 
-            
-        if summary  
-          summary = summary[1].strip
-        else
-          summary = 'no details'
-        end
-        
-        if description  
-          description = description[1].strip
-        else
-          description = summary
-        end
-        
-        if description != summary
-          summary = "#{description} (#{summary})"
-        end
-        
-        time = Time.new()
-        today_start = Time.new(time.year, time.month, time.day)
-        today_end = today_start + 86399
-        thisweek_end = today_start + 604799
-        
-        if (today_start..today_end).cover?(dtstart.to_time()) && (i<max_events)
-              today[today.length] = { summary: summary, startdate: { hour: dtstart.hour%12, minute: "%02d"%dtstart.min, ampm: (dtstart.hour>12?'pm':'am'), year: dtstart.year, month: Date::MONTHNAMES[dtstart.month], day: dtstart.day, dayofweek: Date::DAYNAMES[dtstart.cwday] }, enddate: { hour: dtend.hour%12, minute: "%02d"%dtend.min, ampm: (dtend.hour>12?'pm':'am'), year: dtend.year, month: Date::MONTHNAMES[dtend.month], day: dtend.day, dayofweek: Date::DAYNAMES[dtend.cwday] } }
-              
-        elsif (today_start..thisweek_end).cover?(dtstart.to_time()) && (i<max_events)
-              thisweek[thisweek.length] = { summary: summary, startdate: { hour: dtstart.hour%12, minute: "%02d"%dtstart.min, ampm: (dtstart.hour>12?'pm':'am'), year: dtstart.year, month: Date::MONTHNAMES[dtstart.month], day: dtstart.day, dayofweek: Date::DAYNAMES[dtstart.cwday] }, enddate: { hour: dtend.hour%12, minute: "%02d"%dtend.min, ampm: (dtend.hour>12?'pm':'am'), year: dtend.year, month: Date::MONTHNAMES[dtend.month], day: dtend.day, dayofweek: Date::DAYNAMES[dtend.cwday] } }
-          
-        elsif (i<max_events)
-              later[later.length] = { summary: summary, startdate: { hour: dtstart.hour%12, minute: "%02d"%dtstart.min, ampm: (dtstart.hour>12?'pm':'am'), year: dtstart.year, month: Date::MONTHNAMES[dtstart.month], day: dtstart.day, dayofweek: Date::DAYNAMES[dtstart.cwday] }, enddate: { hour: dtend.hour%12, minute: "%02d"%dtend.min, ampm: (dtend.hour>12?'pm':'am'), year: dtend.year, month: Date::MONTHNAMES[dtend.month], day: dtend.day, dayofweek: Date::DAYNAMES[dtend.cwday] } }
-        end
-        
-        
-        i+=1
-      end
+      cal_file=(Net::HTTP.get 'booking.saltmines.us', '/info/webcal/258B66.ics')
+      calendar = Icalendar.parse(cal_file).first
+      events = filter_upcoming(calendar.events).map { |e| event_to_hash(e) }
+      send_event('events', { events: events.take(4) })
     end
   end
+
+  private
   
+    def event_to_hash(event)
+      {
+        summary: summary(event),
+        date: format_date(event.dtstart),
+        starttime: format_time(event.dtstart),
+        endtime: format_time(event.dtend)
+      }
+    end
 
+    def format_date(date)
+      if date.to_date == Date.today
+        'Today'
+      else
+        date.strftime('%A, %b %-d')   # Tuesday, Jun 23
+      end
+    end
+  
+    def format_time(time)
+      if time.min == 0
+        time.strftime('%-l%P')        # 3pm
+      else
+        time.strftime('%-l:%M%P')     # 3:15pm
+      end
+    end
+  
+    def filter_upcoming(all_events)
+      all_events.select { |e| e.dtend > DateTime.now }
+    end
 
-  send_event('events', { today: today.values[0..3], thisweek: thisweek.values[0..3], later: later.values[0..3] })
+    # event.summary is the creator's name
+    # event.description is the description that they entered.
+    def summary(event)
+      if is_valid_description?(event.description) && event.summary
+        "#{event.summary} - #{event.description}"
+      elsif is_valid_description?(event.description)
+        event.description
+      elsif event.summary
+        event.summary
+      else
+        "no details"
+      end
+    end
+
+    # If no description was entered, SuperSAAS sets the description to the event's URL.
+    # We want to ignore those descriptions.
+    def is_valid_description?(description)
+      description && !description.include?('http://')
+    end
 end
+
+Bookings.new.run
+
