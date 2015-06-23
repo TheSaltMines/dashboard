389c3fd6f7659c1c55efce04dc76f454ac24ddde
Will Koehler <wck555@gmail.com>
2015-06-23 17:37:10 -0400

    Refactor GroupBuzz widget.
    - Move into a class and cleanup logic
    - Convert to a single foreach block
    - Use 'data-addclass-x' to add classes to style older posts
    - Show the 7 most recent topics
    
    At some point we should update buzzbot-read-v2.php to return a single array instead of separate 'today', 'thisweek', and 'earlier' arrays.

--- jobs/groupbuzz.rb
+++ jobs/groupbuzz.rb
@@ -1,32 +1,32 @@
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-require 'net/http'
-require 'json'
-SCHEDULER.every '1m', :first_in => 0 do |job|
-  http = Net::HTTP.new('saltmines.us')
-  response = http.request(Net::HTTP::Get.new("/script/buzzbot-read-v2.php"))
-  json = JSON.parse(response.body)
-  
-  today     = Hash.new({ value: 0 }) 
-  thisweek  = Hash.new({ value: 0 })   
-  earlier   = Hash.new({ value: 0 }) 
-    
-  json['today'].length.times do |i|
-    if i>=0 && i<6  && json['today'][i]['topic'].to_s.length>0
-      today[i-1] = { topic: json['today'][i]['topic'], author: json['today'][i]['author'], contributors: json['today'][i]['contributors'], posts: json['today'][i]['posts'], latest: json['today'][i]['latest'] }
+class GroupBuzz
+
+  def run
+    SCHEDULER.every '1m', :first_in => 0 do |job|
+      http = Net::HTTP.new('saltmines.us')
+      response = http.request(Net::HTTP::Get.new("/script/buzzbot-read-v2.php"))
+      raw_topics = JSON.parse(response.body)
+
+      topics = filter(raw_topics['today']).map { |p| topic_to_hash(p) }
+      topics += filter(raw_topics['thisweek']).map { |p| topic_to_hash(p).merge(thisweek: true) }
+      topics += filter(raw_topics['earlier']).map { |p| topic_to_hash(p).merge(earlier: true) }
+      
+      send_event('groupbuzz', { topics: topics.take(7) })
     end
   end
+
+  private
   
-  json['thisweek'].length.times do |i|
-    if i>=0 && i<6  && json['thisweek'][i]['topic'].to_s.length>0
-      thisweek[i-1] = { topic: json['thisweek'][i]['topic'], author: json['thisweek'][i]['author'], contributors: json['thisweek'][i]['contributors'], posts: json['thisweek'][i]['posts'], latest: json['thisweek'][i]['latest'] }
+    def filter(topics)
+      topics.select { |p| p['topic'].present? }
     end
-  end  
-
-  json['earlier'].length.times do |i|
-    if i>=0 && i<6  && json['earlier'][i]['topic'].to_s.length>0
-      earlier[i-1] = { topic: json['earlier'][i]['topic'], author: json['earlier'][i]['author'], contributors: json['earlier'][i]['contributors'], posts: json['earlier'][i]['posts'], latest: json['earlier'][i]['latest'] }
+    
+    def topic_to_hash(topic)
+      {
+        topic: topic['topic'],
+        contributors: topic['contributors'],
+        posts: topic['posts']
+      }
     end
-  end  
-
-  send_event('groupbuzz', { today: today.values, thisweek: thisweek.values, earlier: earlier.values })
 end
+
+GroupBuzz.new.run
