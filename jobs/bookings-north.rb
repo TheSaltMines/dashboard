efd01b814207a18d800a6f4af471c46f43c563a7
Andy Soell <andy@teamsoell.com>
2015-06-23 10:43:13 -0400

    Added layout for north dashboard
    - Duplicated vertical layout to north layout
    - Duplicated car2go and calendar widgets

---
+++ jobs/bookings-north.rb
@@ -0,0 +1,68 @@
+require 'icalendar'
+
+class Bookings
+
+  def run
+    SCHEDULER.every '1m', :first_in => 0 do |job|
+
+      cal_file=(Net::HTTP.get 'booking.saltmines.us', '/info/webcal/26C631.ics')
+      calendar = Icalendar.parse(cal_file).first
+      events = filter_upcoming(calendar.events).map { |e| event_to_hash(e) }
+      send_event('events', { events: events.take(6) })
+    end
+  end
+
+  private
+
+    def event_to_hash(event)
+      {
+        summary: summary(event),
+        date: format_date(event.dtstart),
+        starttime: format_time(event.dtstart),
+        endtime: format_time(event.dtend)
+      }
+    end
+
+    def format_date(date)
+      if date.to_date == Date.today
+        'Today'
+      else
+        date.strftime('%A, %b %-d')   # Tuesday, Jun 23
+      end
+    end
+
+    def format_time(time)
+      if time.min == 0
+        time.strftime('%-l%P')        # 3pm
+      else
+        time.strftime('%-l:%M%P')     # 3:15pm
+      end
+    end
+
+    def filter_upcoming(all_events)
+      all_events.select { |e| e.dtend > DateTime.now }
+    end
+
+    # event.summary is the creator's name
+    # event.description is the description that they entered.
+    def summary(event)
+      if is_valid_description?(event.description) && event.summary
+        "#{event.summary} - #{event.description}"
+      elsif is_valid_description?(event.description)
+        event.description
+      elsif event.summary
+        event.summary
+      else
+        "no details"
+      end
+    end
+
+    # If no description was entered, SuperSAAS sets the description to the event's URL.
+    # We want to ignore those descriptions.
+    def is_valid_description?(description)
+      description && !description.include?('http://')
+    end
+end
+
+Bookings.new.run
+
