38012dfacace0ec41620e5218d9ccb115ce4c4ff
Joe Shaw <joe@joeshaw.org>
2017-04-06 11:11:35 -0400

    sometimes the now playing track is repeated
    Fetch 5 tracks instead of 4, and slice off either the repeated track or
    the 5th track, so we always get a now playing and 3 previous tracks.

--- jobs/radio.rb
+++ jobs/radio.rb
@@ -3,7 +3,7 @@ require 'net/http'
 require 'rexml/document'
 SCHEDULER.every '5s', :first_in => 0 do |job|
   http = Net::HTTP.new('ws.audioscrobbler.com')
-  response = http.request(Net::HTTP::Get.new("/2.0/?method=user.getrecenttracks&user=thesaltmines&api_key=845d3d4cdeae6e94f8f989e92f1756d0&limit=4&extended=1"))
+  response = http.request(Net::HTTP::Get.new("/2.0/?method=user.getrecenttracks&user=thesaltmines&api_key=845d3d4cdeae6e94f8f989e92f1756d0&limit=5&extended=1"))
   doc = REXML::Document.new(response.body)
 
   tracks = Array.new
@@ -20,6 +20,13 @@ SCHEDULER.every '5s', :first_in => 0 do |job|
   }
 
   np, upcoming = tracks[0], tracks[1..-1]
+
+  # Sometimes the now playing track is repeated
+  if np == upcoming[0]
+    upcoming = upcoming[1..-1]
+  else
+    upcoming = upcoming[0..-2]
+  end
 
   send_event('radio', { npcover: np[:cover], nptrack: np[:track], npartist: np[:artist], upcoming: upcoming})
 end
