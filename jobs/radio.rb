efa2717ada762531decafba19351f4d7f74e3c46
Joshua Davey <josh@joshuadavey.com>
2017-04-06 12:23:57 -0400

    Simplify radio logic

--- jobs/radio.rb
+++ jobs/radio.rb
@@ -6,27 +6,18 @@ SCHEDULER.every '5s', :first_in => 0 do |job|
   response = http.request(Net::HTTP::Get.new("/2.0/?method=user.getrecenttracks&user=thesaltmines&api_key=845d3d4cdeae6e94f8f989e92f1756d0&limit=5&extended=1"))
   doc = REXML::Document.new(response.body)
 
-  tracks = Array.new
+  tracks = []
   doc.elements.each('lfm/recenttracks/track') { |element|
     track = element.elements["name"].text
     artist = element.elements["artist/name"].text
-
-    cover = element.elements["image[@size='large']"].text
-    if cover.nil?
-      cover = element.elements["artist/image[@size='large']"].text
-    end
+    cover = element.elements["image[@size='large']"].text ||
+            element.elements["artist/image[@size='large']"].text
 
     tracks.push({ cover: cover, track: track, artist: artist })
   }
 
-  np, upcoming = tracks[0], tracks[1..-1]
-
   # Sometimes the now playing track is repeated
-  if np == upcoming[0]
-    upcoming = upcoming[1..-1]
-  else
-    upcoming = upcoming[0..-2]
-  end
+  np, *upcoming = tracks.uniq.take(4)
 
   send_event('radio', { npcover: np[:cover], nptrack: np[:track], npartist: np[:artist], upcoming: upcoming})
 end
