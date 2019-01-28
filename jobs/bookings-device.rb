7945d9bc9a880e13fff6c6d6f01b808bd1bee6d4
Joshua Davey <josh@suiteness.com>
2019-01-28 14:59:43 -0500

    Subtract North :sob:

--- jobs/bookings-device.rb
+++
@@ -1,3 +0,0 @@
-# TODO Get URL for device lab calendar
-# Bookings.new(data_id: 'events-device', ical_path: '/info/webcal/26C631.ics', num_events: 5).run
-
--- jobs/bookings-north.rb
+++
@@ -1,2 +0,0 @@
-Bookings.new(data_id: 'events-north', ical_path: '/info/webcal/26C631.ics', num_events: 5).run
-
